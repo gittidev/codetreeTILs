92121caba66be56d93bac6d6178c0432543564cd
Gitti <qhfka961@gmail.com>
2024-12-06 22:11:04 +0900

    Title: 비교 연산, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241206/비교 연산/comparison-operator.py
@@ -0,0 +1,33 @@
+a,b = map(int, input().split());
+
+flag = 0
+
+if a>=b:
+    print(1)
+else: 
+    print(0)
+
+if a>b:
+    print(1)
+else:
+    print(0)
+
+if b>=a:
+    print(1)
+else :
+    print(0)
+
+if b>a:
+    print(1)
+else:
+    print(0)
+
+if a==b:
+    print(1)
+else:
+    print(0)
+
+if a!=b:
+    print(1)
+else:
+    print(0)
