c717c9adc5894fc134e5ff3b0d5829f8459e1791
Gitti <qhfka961@gmail.com>
2024-12-06 21:23:54 +0900

    Title: 살 수 있는 물건, Time: 174ms, Memory: 15MB, Status: Passed - CodeTree

--- 241206/살 수 있는 물건/things-able-to-buy.js
+++ 241206/살 수 있는 물건/things-able-to-buy.js
@@ -5,7 +5,7 @@ let n = parseInt(fs.readFileSync(0));
 if (n>=3000) {
     console.log('book')
 } else if (n>=1000) {
-    console.log("masl")
+    console.log("mask")
 } else {
     console.log("no")
 }
