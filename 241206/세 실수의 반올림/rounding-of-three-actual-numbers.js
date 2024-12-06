e716ac1da3cb7e6295360db71ca7aa7a172e4d60
Gitti <qhfka961@gmail.com>
2024-12-06 16:31:02 +0900

    Title: 세 실수의 반올림, Time: 177ms, Memory: 15MB, Status: Passed - CodeTree

--- 241206/세 실수의 반올림/rounding-of-three-actual-numbers.js
+++ 241206/세 실수의 반올림/rounding-of-three-actual-numbers.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
-let [a,b,c] =  fs.readFileSync(0).toString().split("\n").map(item => item.toFixed(3))
+let [a,b,c] =  fs.readFileSync(0).toString().split("\n").map(item => Number(item))
 
-console.log(a)
-console.log(b)
-console.log(c)
+console.log(a.toFixed(3))
+console.log(b.toFixed(3))
+console.log(c.toFixed(3))
