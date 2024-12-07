9595de908369f737f117c14508d1dc03f13e613a
Gitti <qhfka961@gmail.com>
2024-12-07 10:10:27 +0900

    Title: 특정 조건 세 정수 비교, Time: 191ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241207/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
+++ 241207/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
@@ -5,11 +5,11 @@ let [a,b,c] = fs.readFileSync(0).toString().trim().split('\n').map(i=>Number(i))
 let result1 = 0
 let result2 = 0
 
-if (a<=a && a<=b && a<=c) {
+if (a<=b && a<=c) {
     result1=1
 } 
 
-if (a===b &&  b===c && a===c) {
+if (a===b &&  b===c ) {
     result2=1
 }
 
