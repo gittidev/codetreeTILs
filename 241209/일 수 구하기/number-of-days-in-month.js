c6798e422c3cd5820826c4e75cd5616cda2336da
Gitti <qhfka961@gmail.com>
2024-12-09 13:32:56 +0900

    Title: 일 수 구하기, Time: 183ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241209/일 수 구하기/number-of-days-in-month.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+const n = Number(fs.readFileSync(0).toString().trim());
+if (n==2) {
+    console.log(28)
+} else if (n<=7)  {
+    if (n%2==1)  {
+        console.log(31)
+    } else {
+        console.log(30)
+    }
+} else  {
+    if (n%2==0) {
+        console.log(31)
+    } else {
+        console.log(30)
+    }
+}
