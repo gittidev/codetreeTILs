135e71f4d67e44e3135aebb1241341b3917ac425
Gitti <qhfka961@gmail.com>
2024-12-09 22:28:17 +0900

    Title: 2 또는 3의 배수, Time: 177ms, Memory: 15MB, Status: Passed - CodeTree

--- 241209/2 또는 3의 배수/multiple-of-2-or-3.js
+++ 241209/2 또는 3의 배수/multiple-of-2-or-3.js
@@ -4,7 +4,7 @@ let n = Number(fs.readFileSync(0).toString().trim());
 let result = [];
 
 
-for (i=0; i<8; i++) {
+for (i=0; i<n; i++) {
     if  ((i+1)%2==0 || (i+1)%3==0) {
         result[i] = 1
     }  else {
