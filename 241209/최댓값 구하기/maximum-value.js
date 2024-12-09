244526b9a81b91319a1be73d721f1b3f2d7771ff
Gitti <qhfka961@gmail.com>
2024-12-09 13:35:53 +0900

    Title: 최댓값 구하기, Time: 182ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241209/최댓값 구하기/maximum-value.js
@@ -0,0 +1,18 @@
+const fs  =  require("fs");
+
+let [a,b,c]  =  fs.readFileSync(0).toString().trim().split(' ').map(i=> Number(i));
+
+let Max = 0;
+
+if (a>Max) {
+    Max=a
+} 
+
+if (b>Max) {
+    Max=b
+} 
+if (c>Max) {
+    Max=c
+}
+
+console.log(Max)
