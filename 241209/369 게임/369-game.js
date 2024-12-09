992bba9485780e24d9105cd60e4cefb46b13ce59
Gitti <qhfka961@gmail.com>
2024-12-09 23:09:32 +0900

    Title: 369 게임, Time: 165ms, Memory: 14MB, Status: Runtime Error - CodeTree

--- 241209/369 게임/369-game.js
+++ 241209/369 게임/369-game.js
@@ -1,19 +1,32 @@
 const fs = require("fs");
 
 let n = Number(fs.readFileSync(0).toString().trim());
+let arr = [3,6,9]
 
-let num = [3,6,9]
+
 
 result = [];
 let i = 1
 while (i<=n) {
-    if ((i%3==0) || (num.includes(i))) {
+
+    if (i>=10) {
+        i.toString().split("") 
+        for (j=0; j<=i.length(); j++) {
+            if (arr.includes(j)) {
+                result.push(0)
+                return
+            }
+        }
+        i++
+    } else if (i%3==0) {
         result.push(0)
         i++
     } else {
-        result.push(i)
+   result.push(i)
         i++
     }
+
+
 }
 
 console.log(result.join(" "))
