4929032c53c1fee3a19588323ce94c4686f99be5
Gitti <qhfka961@gmail.com>
2024-12-10 00:25:29 +0900

    Title: 홀수만 더하기, Time: 173ms, Memory: 15MB, Status: Passed - CodeTree

--- 241210/홀수만 더하기/add-only-odd-numbers.js
+++ 241210/홀수만 더하기/add-only-odd-numbers.js
@@ -1,13 +1,14 @@
 const fs = require('fs');
 
-let arr = fs.readFileSync(0).toString().trim().split("n").map(i=>Number(i));
-
+let arr = fs.readFileSync(0).toString().trim().split("\n").map(i=>Number(i));
 let n = arr.shift()
 let Sum=0
 
+
+
 for (let i = 0; i<n;  i++) {
     let x= arr[i];
-    if (x%3==0 && x%2==1) {
+    if (x%3==0 && x%2!=0) {
         Sum+=x
     }    
 }
