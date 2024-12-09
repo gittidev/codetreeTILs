1aa9a56c3d3d2a8caf76fac2bbda630b31e6b8d7
Gitti <qhfka961@gmail.com>
2024-12-09 23:54:41 +0900

    Title: 청소당번 정하기, Time: 164ms, Memory: 15MB, Status: Passed - CodeTree

--- 241209/청소당번 정하기/cleaning-numbering.js
+++ 241209/청소당번 정하기/cleaning-numbering.js
@@ -4,12 +4,12 @@ let n = Number(fs.readFileSync(0).toString().trim());
 
 let [a,b,c] = [0,0,0];
 
-for (let i =0 ; i<n; i++) {
-    if (i%12 == 0 &&  i!=0) {
+for (let i =1 ; i<=n; i++) {
+    if (i%12 == 0) {
         c++
-    } else if (((i%2==0 && i%3==0) || i%3==0) && i!=0) {
+    } else if (i%3==0) {
         b++
-    } else if  (i%2==0 && i!=0) {
+    } else if  (i%2==0) {
         a++
     }
 }
