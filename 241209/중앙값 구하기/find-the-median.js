662f7d149483486b10f2fc6331d87d0507724164
Gitti <qhfka961@gmail.com>
2024-12-09 13:52:56 +0900

    Title: 중앙값 구하기, Time: 174ms, Memory: 15MB, Status: Passed - CodeTree

--- 241209/중앙값 구하기/find-the-median.js
+++ 241209/중앙값 구하기/find-the-median.js
@@ -4,12 +4,22 @@ let [a,b,c] = fs.readFileSync(0).toString().trim().split(" ").map(i=>Number(i));
 
 let middle =  0;
 
-if ((a>b && c>a) || (b>a &&  a>c)) {
-    middle=a
-} else if ((b>a && c>b)  || (a>b&&  b>c)) {
-    middle=b
-} else if ((c>b && a>c) || (c>b&&  a>c)){
-    middle=c
+if (a>b)  {
+    if (a<c) {
+        middle=a
+    }  else if (b>c) {
+        middle=b
+    } else {
+        middle=c
+    }
+} else {
+    if (a>c) {
+        middle=a
+    } else if (b<c) {
+        middle=b
+    } else {
+        middle=c
+    }
 }
 
 console.log(middle)
